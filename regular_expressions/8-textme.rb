d3a0101f1b4960562194ec32dd0d35bd05b057d0
Christian-Regnante <c.ngwino@alustudent.com>
2025-05-09 15:18:06 -0700

    Regex 8-textme.rb extracting text scripts from a file

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,9 +1,4 @@
 #!/usr/bin/env ruby
 
-# The sender info
-puts ARGV[0].scan(/\Dfrom:(\W\d+|[a-zA-Z]+)\D/).join
-# The Receiver info
-puts ARGV[0].scan(/\Dto:(\W\d+|[a-zA-Z]+)\D/).join
-# The flags
-puts ARGV[0].scan(/\Dflags:(.\d|\d):(.\d|\d):(.\d|\d):(.\d|\d):(.\d|\d)\D/).join
+puts ARGV[0].scan(/from:(\W\d+|[a-zA-Z]+)/,/to:(\W\d+|[a-zA-Z]+)/,/flags:(.\d|\d):(.\d|\d):(.\d|\d):(.\d|\d):(.\d|\d)/).join
 
